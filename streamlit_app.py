cad7ba95ab31c5de0400dc8c8ecb3f197e167418
haiden <86491628+haidendr@users.noreply.github.com>
2022-07-13 22:38:07 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,5 +7,7 @@ st.write('🥬kale, Spinach & Rocket Smoothie')
 st.write('🥚Hard-Boiled Free-Range Egg')
 st.write('🥑🍞Avocado Toast')
 
+st.title('🍌🍍Build Your Own Fruit Smoothie🍓🥭')
+
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 st.dataframe(my_fruit_list)
