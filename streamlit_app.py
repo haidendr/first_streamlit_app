64a53d2064cbbc53eabc28518132243a1550a6f4
haiden <86491628+haidendr@users.noreply.github.com>
2022-07-13 22:35:10 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,11 @@
 import streamlit as st
 import pandas
 
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-st.dataframe(my_fruit_list)
-
 st.title('Breakfast Favorites')
 st.write('🥣Omega 3 & Blueberry Oatmeal')
 st.write('🥬kale, Spinach & Rocket Smoothie')
 st.write('🥚Hard-Boiled Free-Range Egg')
 st.write('🥑🍞Avocado Toast')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+st.dataframe(my_fruit_list)
