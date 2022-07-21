5aea2490ac9898d5b4d4ca20fefeffffbeba0242
haiden <86491628+haidendr@users.noreply.github.com>
2022-07-21 14:37:43 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,35 +1,35 @@
-import streamlit as st
+import streamlit
 import pandas
 
-st.title('Breakfast Favorites')
-st.write('🥣Omega 3 & Blueberry Oatmeal')
-st.write('🥬kale, Spinach & Rocket Smoothie')
-st.write('🥚Hard-Boiled Free-Range Egg')
-st.write('🥑🍞Avocado Toast')
+streamlit.title('Breakfast Favorites')
+streamlit.write('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.write('🥬kale, Spinach & Rocket Smoothie')
+streamlit.write('🥚Hard-Boiled Free-Range Egg')
+streamlit.write('🥑🍞Avocado Toast')
 
-st.title('🍌🍍Build Your Own Fruit Smoothie🍓🥭')
+streamlit.title('🍌🍍Build Your Own Fruit Smoothie🍓🥭')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Let's put a pick list here so they can pick the fruit they want to include
-fruits_selected = st.multiselect("Pick some fruits:", list(my_fruit_list.index),['Strawberries', 'Banana'])
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Strawberries', 'Banana'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # display the table on the page
-st.dataframe(fruits_to_show)
+streamlit.dataframe(fruits_to_show)
 
 #New Section to display fruityvice api response
-st.header('Fruityvice Fruit Advice!')
+streamlit.header('Fruityvice Fruit Advice!')
 
-fruit_choice = st.text_input('What fruit would you like information about?', 'Kiwi')
-st.write('The user entered', fruit_choice)
+fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
+streamlit.write('The user entered', fruit_choice)
 
 import requests
-fruityvice_response = request.get("https://fruityvice.com/api/fruit/" + "fruit_choice")
+fruityvice_response = request.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 # take the json version of the response and normalize it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 
 # output the screen as a table
-st.dataframe(fruityvice_normalized)
+streamlit.dataframe(fruityvice_normalized)
