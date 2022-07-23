c24f2e2fcc398d76b630b9815390b9502db4208c
haiden <86491628+haidendr@users.noreply.github.com>
2022-07-23 14:36:55 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,6 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # display the table on the page
 streamlit.dataframe(fruits_to_show)
 
-
 #create the repeatable code block (called a function)
 def get_fruityvice_data(this_fruit_choice):
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
@@ -44,9 +43,6 @@ try:
 except URLError as e:
       streamlit.error()
 
-#don't run anything past here while we troubleshoot
-streamlit.stop()
-
 streamlit.header("The fruit load list contains:")
 #Snowflake-related functions
 def get_fruit_load_list():
@@ -59,6 +55,9 @@ if stremlit.button('Get Fruit Load List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows = get_fruit_load_list()    
     streamlit.dataframe(my_data_rows)
+
+#don't run anything past here while we troubleshoot
+streamlit.stop()
 
 #allow the end user to add a fruit to the list
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
